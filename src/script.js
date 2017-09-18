a7f636d15517332b97ddcf70fb4c60f1285262f5
DennisWanjiru <denniswanjiru71@gmail.com>
2017-09-18 12:35:45 +0300

    Css part completion

--- src/script.js
+++ src/script.js
@@ -3,5 +3,12 @@
 var handlers = {
 	toggleBtn: function () {
 		var scale = document.querySelector("#scale-demo").classList.toggle("scale-out");
+	},
+
+	scrollUpBtn: function () {
+		var height = window.innerHeight;
+		if (height > 700) {
+			document.querySelector(".fixed-action-btn").classList.add('hide');
+		}
 	}
 }
